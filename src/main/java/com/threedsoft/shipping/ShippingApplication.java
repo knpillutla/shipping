030ac8719bbfba8e9f95d189854d57d536d3fa29
Krishna Pillutla <knpillutla@gmail.com>
2018-11-05 22:13:35 -0500

    adding

--- src/main/java/com/threedsoft/shipping/ShippingApplication.java
+++ src/main/java/com/threedsoft/shipping/ShippingApplication.java
@@ -8,6 +8,9 @@ import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import com.threedsoft.shipping.streams.ShippingStreams;
 import com.threedsoft.util.service.EventPublisher;
@@ -31,4 +34,19 @@ public class ShippingApplication {
 	public EventPublisher eventPublisher() {
 		return new EventPublisher(shippingStreams.outboundShip());
 	}	
+	@Bean
+	public CorsFilter corsFilter() {
+
+	    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	    CorsConfiguration config = new CorsConfiguration();
+	    config.setAllowCredentials(true); 
+	    config.addAllowedOrigin("http://*the3dsoft.com");
+	    config.addAllowedOrigin("http://localhost");
+	    config.addAllowedOrigin("https://localhost:5000");
+	    config.addAllowedHeader("*");
+	    config.addAllowedMethod("*");
+	    source.registerCorsConfiguration("/**", config);
+	    return new CorsFilter(source);
+	}
+	
 }
